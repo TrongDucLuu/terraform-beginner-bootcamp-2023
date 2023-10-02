46ac9c122f876a3de5de916b28b020788c79bbe7
TrongDucLuu <116299897+TrongDucLuu@users.noreply.github.com>
2023-10-03 00:44:40 +0700

    #21 Restructure Root Module, add tagging for S3 bucket (#22)

--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-terraform {
-  cloud {
-    organization = "ducluu"
-
-    workspaces {
-      name = "Terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.19.0"
-    }
-  }
-}
-provider "aws" {
-  # Configuration options
-}
-provider "random" {
-  # Configuration options
-}
 
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
@@ -37,7 +12,7 @@ resource "aws_s3_bucket" "example" {
   # Bucket Naming Rules
   #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = random_string.bucket_name.result
-}
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
